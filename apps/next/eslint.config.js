9d8c8164692c5249d6fffd98c21a1b00ba9bbd86
Nikita Povalishev <np@skl.vc>
2025-08-09 00:03:48 +0300

    Init commit

---
+++ apps/next/eslint.config.js
@@ -0,0 +1,4 @@
+import { nextJsConfig } from "@sandbox/config-eslint/nextjs";
+
+/** @type {import("eslint").Linter.Config} */
+export default nextJsConfig;
---
+++ apps/react/eslint.config.js
@@ -0,0 +1,4 @@
+import { config } from "@sandbox/config-eslint/react";
+
+/** @type {import("eslint").Linter.Config} */
+export default config;
